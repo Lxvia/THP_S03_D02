5086583d2e4b0b6ce0c1fe5cfd25e2390a0b07f9
Olivia <oliviarudolf67@gmail.com>
2022-04-19 19:06:05 +0200

    Après 5h de galère

--- lib/multiple_numbers.rb
+++ lib/multiple_numbers.rb
@@ -1,38 +1,23 @@
+
+
 def is_multiple_of_3_or_5?(current_number)
-  current_number %=3 == 0 || current_number %=5 == 0 
-end
-
-def sum_of_3_and_5_multiples(final_number)
-    final_sum = 0
-    if current_number.is.a? Float || String 
-      puts "Ceci n'est pas une bonne réponse"
-    end
-    # return mets fin à la fonction, ajouter une règle qui permet de filtrer selon le test
-  for current_number in (0... final_number)
-    if is_multiple_of_3_or_5?(current_number)
-      final_sum = current_number + final_sum
-    end
+    current_number %3 == 0 || current_number %5 == 0 
   end
-  return final_sum
+  
+def sum_of_3_and_5_multiples(final_number)
+      final_sum = []
+  if final_number.to_i >= 0 && final_number.is_a?(Integer)  
+      for i in (0...final_number.to_i)
+          if is_multiple_of_3_or_5?(i)
+              final_sum << i
+          end
+      end
+      return final_sum.sum
+  else
+      return "Ceci n'est pas une bonne réponse"
+  end
 end
 
-#  if is_multiple_of_3_or_5?(current_number)
-#    return true
-#  end
-
-
-
-
-
-  #    return current_number.upto(0..1000).select{|value| value%3 == 0 || value%5 == 0}
-  #  else
-    
-  #  end
-
-#end
-
-#def sum_of_3_and_5_multiples(final_number)
-#  current_number = current_number.upto(1000)
-#return current_number
-#return final_sum
-#end 
+puts sum_of_3_and_5_multiples(11)
+  
+ 
